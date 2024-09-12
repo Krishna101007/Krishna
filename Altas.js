674cae7e7b6a1a30063f3d53f5012a43246a3c81
Krishna101007 <krishnagupta101007@gmail.com>
2024-09-12 14:04:46 +0530

    Add files via upload

---
+++ Altas.js
@@ -0,0 +1,124 @@
+var b1=document.querySelector(".col-btn");
+			var a=document.querySelector("#str")
+			var a2=document.querySelector("#str2")
+			var a3=document.querySelector("#str3")
+			var a4=document.querySelector("#str4")
+			var a5=document.querySelector("#str5")
+			var a6=document.querySelector("#str6")
+			var b2=document.querySelector(".col-btn2");
+			var b3=document.querySelector(".col-btn3");
+			var b4=document.querySelector(".col-btn4");
+			var b5=document.querySelector(".col-btn5");
+			var b6=document.querySelector(".col-btn6");
+			var c=document.querySelector(".img")
+			b1.addEventListener("click",function(){
+				a.style.color="white"
+				a.style.fontWeight="bold"
+				a2.style.fontWeight="200"
+				a3.style.fontWeight="200"
+				a4.style.fontWeight="200"
+				a5.style.fontWeight="200"
+				a6.style.fontWeight="200"
+				a.style.transition="1s"
+				b1.style.backgroundColor="#dcdde1"
+				b2.style.backgroundColor="#c9bfb3"
+				b4.style.backgroundColor="#997459"
+				b4.style.backgroundColor=" #ffffff"
+				b5.style.backgroundColor="#b6565c"
+				b6.style.backgroundColor="#222222"
+				c.src="pearl.jpg"
+			})
+
+			b2.addEventListener("click",function(){
+				a2.style.color="bold"
+				a2.style.fontWeight="500"
+				a.style.fontWeight="200"
+				a3.style.fontWeight="200"
+				a4.style.fontWeight="200"
+				a5.style.fontWeight="200"
+				a6.style.fontWeight="200"
+				a2.style.transition="1s"
+				b1.style.backgroundColor="#dcdde1"
+				b2.style.backgroundColor="#c9bfb3"
+				b5.style.backgroundColor="#997459"
+				b4.style.backgroundColor="#ffffff"
+				b5.style.backgroundColor="#b6565c"
+				b6.style.backgroundColor="#222222"
+				c.src="amber.jpg"
+			})
+
+			b3.addEventListener("click",function(){
+				a3.style.color="bold"
+				a3.style.fontWeight="500"
+				a.style.fontWeight="200"
+				a2.style.fontWeight="200"
+				a4.style.fontWeight="200"
+				a5.style.fontWeight="200"
+				a6.style.fontWeight="200"				
+				a3.style.transition="1s"
+				b1.style.backgroundColor="#dcdde1"
+				b2.style.backgroundColor="#c9bfb3"
+				b6.style.backgroundColor="#997459"
+				b4.style.backgroundColor="#ffffff"
+				b5.style.backgroundColor="#b6565c"
+				b6.style.backgroundColor="#222222"
+				c.src="brown.jpg"
+			})
+
+			b4.addEventListener("click",function(){
+				a4.style.color="bold"
+				a4.style.fontWeight="500"
+				a.style.fontWeight="200"
+				a2.style.fontWeight="200"
+				a3.style.fontWeight="200"
+				a5.style.fontWeight="200"
+				a6.style.fontWeight="200"				
+				a4.style.transition="1s"
+				b1.style.backgroundColor="#dcdde1"
+				b2.style.backgroundColor="#c9bfb3"
+				b3.style.backgroundColor="#997459"
+				b4.style.backgroundColor="#ffffff"
+				b5.style.backgroundColor="#b6565c"
+				b6.style.backgroundColor="#222222"
+				c.src="white.jpg"
+			})
+
+			b5.addEventListener("click",function(){
+				a5.style.color="bold"
+				a5.style.fontWeight="500"
+				a.style.fontWeight="200"
+				a2.style.fontWeight="200"
+				a3.style.fontWeight="200"
+				a4.style.fontWeight="200"
+				a6.style.fontWeight="200"				
+				a5.style.transition="1s"
+				b1.style.backgroundColor="#dcdde1"
+				b2.style.backgroundColor="#c9bfb3"
+				b3.style.backgroundColor="#997459"
+				b4.style.backgroundColor="#ffffff"
+				b5.style.backgroundColor="#b6565c"
+				b6.style.backgroundColor="#222222"
+				c.src="red.jpg"
+			})
+
+			b6.addEventListener("click",function(){
+				a6.style.color="bold"
+				a6.style.fontWeight="500"
+				a.style.fontWeight="200"
+				a2.style.fontWeight="200"
+				a3.style.fontWeight="200"
+				a4.style.fontWeight="200"
+				a5.style.fontWeight="200"
+				a6.style.transition="1s"
+				b1.style.backgroundColor="#dcdde1"
+				b2.style.backgroundColor="#c9bfb3"
+				b3.style.backgroundColor="#997459"
+				b4.style.backgroundColor="#ffffff"
+				b5.style.backgroundColor="#b6565c"
+				b6.style.backgroundColor="#222222"
+				c.src="black.jpg"
+			})
+
+
+
+			
